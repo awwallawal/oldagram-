fe9154e178a32cb84a94aed924b122b6927dded3
awwallawal <103115087+awwallawal@users.noreply.github.com>
2023-01-09 18:32:53 +0100

    Update app.js
    Added the lines of JS codes to make the heart icons in the three columns interactive

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 let count = 0;
-let likenumber = document.getElementById('like-number');
-let heart = document.getElementById('heart');
+let likenumber1 = document.getElementById('like-number-1');
+let heart1 = document.getElementById('heart-1');
 
-heart.addEventListener('click', function(){
+heart1.addEventListener('click', function(){
  count +=1;
- likenumber.textContent = count;
-})
+ likenumber1.textContent = count;
+});
+
+
+let number2 = 0;
+let likenumber2 = document.getElementById('like-number-2');
+let heart2 = document.getElementById('heart-2');
+
+heart2.addEventListener('click', function(){
+ number2 +=1;
+ likenumber2.textContent = number2;
+});
+
+
+let number3 = 0;
+let likenumber3 = document.getElementById('like-number-3');
+let heart3 = document.getElementById('heart-3');
+
+heart3.addEventListener('click', function(){
+ number3 += 1; 
+ likenumber3.textContent = number3;
+});
